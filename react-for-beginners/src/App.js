10166f5bdc32ea79727ce9766c8e356eacb88502
JNYLee <junyjuny1021@gmail.com>
2021-11-21 22:49:02 +0900

    [movieRate/init]: 컴포넌트 분리하기

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,51 +1,16 @@
 import { useState, useEffect } from "react";
+import Movie from "./components/Movie";
+import PropTypes from "prop-types";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  // useEffect(() => {
-  //   fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
-  //   .then((response) => response.json())
-  //   .then((json) => {
-  //     setMovies(json.data.movies)
-  //     setLoading(false);
-  //   });
-  // }, []);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-              </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
+
+Movie.propTypes ={
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default App;
