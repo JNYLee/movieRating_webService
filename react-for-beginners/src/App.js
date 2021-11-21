02b631a802f00363fc045042fda17f18f984e9fd
JNYLee <junyjuny1021@gmail.com>
2021-11-21 17:11:02 +0900

    [useEffect/init]: 1번 만실행시키기

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,29 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onChange = (event) => setKeyword(event.target.value);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
+  useEffect(() => {
+    if(keyword !== "" && keyword.length > 5){
+      console.log("Search For", keyword);
+    }
+  }, [keyword]);
   return (
     <div>
-      <h1  className={styles.title}>Welcome back React!</h1>
-     <Button text={"Continue"} />
+      <input 
+      onChange={onChange}
+      value = {keyword}
+      placeholder = {"Search for..."}
+      type="text"
+      />
+      <h1>{counter}</h1>
+     <button onClick={onClick}>Click me</button>
     </div>
   );
 }
