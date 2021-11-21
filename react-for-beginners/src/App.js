ef0cf36a402213c0a51c3994f28d869c6926da05
JNYLee <junyjuny1021@gmail.com>
2021-11-21 17:38:25 +0900

    [useEffect/init]: cleanUp 함수 배우기

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,29 +1,21 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onChange = (event) => setKeyword(event.target.value);
-  const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time");
+function Hello () {
   useEffect(() => {
-    console.log("CALL THE API...");
+    console.log("created :)");
+    return () => console.log("destroyed :("); // 헷갈림
   }, []);
-  useEffect(() => {
-    if(keyword !== "" && keyword.length > 5){
-      console.log("Search For", keyword);
-    }
-  }, [keyword]);
+  return <h1>Hello</h1>
+}
+
+function App() {
+ const [showing, setShowing] = useState(false);
+ const onClick = () => setShowing((prev) =>  !prev);
+
   return (
     <div>
-      <input 
-      onChange={onChange}
-      value = {keyword}
-      placeholder = {"Search for..."}
-      type="text"
-      />
-      <h1>{counter}</h1>
-     <button onClick={onClick}>Click me</button>
+     {showing ? <Hello /> : null}
+     <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
